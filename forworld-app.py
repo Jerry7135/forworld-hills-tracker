ebe188bc25a38b88dc031ef65f121fff35073a1b
Jerry7135 <35686731+Jerry7135@users.noreply.github.com>
2026-08-21 11:25:43 +0800

    Update forworld-app.py

--- forworld-app.py
+++ forworld-app.py
@@ -220,6 +220,10 @@ if not df.empty:
     # 🖼️ 【精選輪播照片區】: 放 1~3 張最精華的照片，網頁直接顯示
     MILESTONE_PHOTOS = {
 
+        "地上12樓頂版勘驗": [
+            {"url": "https://raw.githubusercontent.com/Jerry7135/forworld-hills-tracker/main/photos/12f-a.jpg", "tab_name": "📸 260815街拍", "caption": "路口空拍視角(芳鄰-Jerry提供)"}
+        ],
+
         "地上11樓頂版勘驗": [
             {"url": "https://raw.githubusercontent.com/Jerry7135/forworld-hills-tracker/main/photos/11f-a.jpg", "tab_name": "📸 260712空拍", "caption": "路口空拍視角(芳鄰-丘丘蛙提供)"}
         ],
